87075e6e2a490ad77a1801a12f7de7a8976225af
Гуртовой Алексей Сергеевич <zodiac1993@ya.ru.ru>
2026-02-19 18:51:03 +0300

    fix ci

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,20 +1,17 @@
 terraform {
   backend "s3" {
     endpoints = {
-      s3  = "https://s3.owgrant.su"
-      sts = "https://s3.owgrant.su"
-      iam = "https://s3.owgrant.su"
+      s3 = "https://s3.owgrant.su"
     }
     bucket     = "otus"
-    region     = "us-east-1" # MinIO требует регион, даже фиктивный
-    key        = "state/airflow.tfstate" # Путь к файлу состояния
+    region     = "us-east-1"
+    key        = "state/airflow.tfstate"
     
-    # Чтобы работало с MinIO (самоподписанные сертификаты или http)
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
-    use_path_style              = true # instead of force_path_style
+    use_path_style              = true
     skip_s3_checksum            = true
   }
   required_version = ">= 1.0.0"
