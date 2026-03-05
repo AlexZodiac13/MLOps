1ed81a6a7dc11cfe7e6e581f667856f86917dd3e
Гуртовой Алексей Сергеевич <zodiac1993@ya.ru.ru>
2026-03-05 20:21:40 +0300

    add test infra

---
+++ infra/terraform/tests/main_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformInfrastructure(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Путь до папки с конфигурацией Terraform
+		TerraformDir: "../",
+
+		// Переменные окружения, если нужно
+		// EnvVars: map[string]string{
+		// 	"TF_VAR_yc_token": "your-token", // передавать лучше через GitLab ENV
+		// },
+	})
+
+	// Запуск terraform init и terraform plan (без развертывания ресурсов)
+	// Это проверяет синтаксис и доступность модулей/провайдеров
+	terraform.InitAndPlan(t, terraformOptions)
+}
